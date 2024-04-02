7f9766170bb0cc0a571e78884577e8206f4781da
Timofey Latypov <timtompson3241@gmail.com>
2024-04-02 10:16:20 +0300

    lab 3.2 WIP
    simPy implementation (Вопросы по тому как рассчитывать характеристики по результатам работы этой библиотеки, поэтому вся математика лежит на смо хендлере)

--- Lab_3/SMO_handler.py
+++ Lab_3/SMO_handler.py
@@ -1,71 +1,63 @@
 from SMO import *
 
+
 class SMO_handler:
-    
-    def __init__(self,serviceTime, intensity, intensityservice=0):
-        self.smo = SMO(intensity,serviceTime, intensityservice)
+
+    def __init__(self, serviceTime, intensity, intensityservice=0):
+        self.smo = SMO(intensity, serviceTime, intensityservice)
+
     def SingleChannelWithFail(self):
         self.smo.SingleChannelWithFail()
-        return ("Вероятность обслуживания: " + str(self.smo.ProbabilityService) + "\n" + 
-                "Вероятность отказа: " + str(self.smo.ProbabilityFault) + "\n" + 
-                "Время простоя: " + str(self.smo.DownTime) + "\n"+ 
-                "Пропускная способность: " + str(self.smo.Bandwidth) + "\n")
+        return ("Вероятность обслуживания: " + "{:2f}".format(self.smo.ProbabilityService) + "\n" +
+                "Вероятность отказа: " + "{:2f}".format(self.smo.ProbabilityFault) + "\n" +
+                "Время простоя: " + "{:2f}".format(self.smo.DownTime) + "\n" +
+                "Пропускная способность: " + "{:2f}".format(self.smo.Bandwidth) + "\n")
 
-    
-        
     def SingleChannelWithQueue(self, queue_length,):
         self.smo.SingleChannelWithQueue(queue_length)
-        return ("Вероятность обслуживания: " + str(self.smo.ProbabilityService) + "\n" + 
-                "Вероятность отказа: " + str(self.smo.ProbabilityFault) + "\n" + 
-                "Абсолютная пропускная способность: " + str(self.smo.RelativeBandwidth) + "\n" +
-                "Среднее число занятых каналов: " + str(self.smo.AverageChannel) + "\n" +
-                "Средняя длина очереди: " + str(self.smo.QueueLength) + "\n" +
-                "Среднее время ожидания в очереди: " + str(self.smo.WaitTime) + "\n"+
-                "Среднее число заявок в СМО: " + str(self.smo.Lsmo) + "\n" +
-                "Cреднее время нахождения заявки в СМО: " + str(self.smo.SMOTime) + "\n")
-    
-            
-            
+        return ("Вероятность обслуживания: " + "{:2f}".format(self.smo.ProbabilityService) + "\n" +
+                "Вероятность отказа: " + "{:2f}".format(self.smo.ProbabilityFault) + "\n" +
+                "Абсолютная пропускная способность: " + "{:2f}".format(self.smo.RelativeBandwidth) + "\n" +
+                "Среднее число занятых каналов: " + "{:2f}".format(self.smo.AverageChannel) + "\n" +
+                "Средняя длина очереди: " + "{:2f}".format(self.smo.QueueLength) + "\n" +
+                "Среднее время ожидания в очереди: " + "{:2f}".format(self.smo.WaitTime) + "\n" +
+                "Среднее число заявок в СМО: " + "{:2f}".format(self.smo.Lsmo) + "\n" +
+                "Cреднее время нахождения заявки в СМО: " + "{:2f}".format(self.smo.SMOTime) + "\n")
+
     def SingleChannelWithQueueWhithoutLen(self):
         self.smo.SingleChannelWithQueueWhithoutLen()
-        if(self.smo.IsStatic):
-            return ("Средняя длина очереди: " + str(self.smo.QueueLength) + "\n" +
-                    "Среднее время ожидания в очереди: " + str(self.smo.WaitTime) + "\n"+
-                    "Среднее число заявок в СМО: " + str(self.smo.Lsmo) + "\n" +
-                    "Cреднее время нахождения заявки в СМО: " + str(self.smo.SMOTime) + "\n")
+        if (self.smo.IsStatic):
+            return ("Средняя длина очереди: " + "{:2f}".format(self.smo.QueueLength) + "\n" +
+                    "Среднее время ожидания в очереди: " + "{:2f}".format(self.smo.WaitTime) + "\n" +
+                    "Среднее число заявок в СМО: " + "{:2f}".format(self.smo.Lsmo) + "\n" +
+                    "Cреднее время нахождения заявки в СМО: " + "{:2f}".format(self.smo.SMOTime) + "\n")
         else:
-            return "Стационарное состояние невозможно\n"   
-            
-            
-    def MultiChannelWithFail(self, channel_count):
-        self.smo.MultiChannelWithFail(channel_count)
-        return ("Вероятность обслуживания: " + str(self.smo.ProbabilityService) + "\n" + 
-                "Вероятность отказа: " + str(self.smo.ProbabilityFault) + "\n" + 
-                "Относительнаяпропускная способность: " + str(self.smo.RelativeBandwidth) + "\n" +
-                "Абсолютная пропускная способность: " + str(self.smo.Bandwidth) + "\n"+
-                "Среднее число занятых каналов: " + str(self.smo.AverageChannel) + "\n")
-        
-            
-    def MultiChannelWithQueue(self, channel_count,queue_length):
-        
-        self.smo.MultiChannelWithQueue(channel_count,queue_length)
-        return ("Вероятность отказа: " + str(self.smo.ProbabilityFault) + "\n" + 
-                "Среднее число занятых каналов: " + str(self.smo.AverageChannel) + "\n" +
-                "Средняя длина очереди: " + str(self.smo.QueueLength) + "\n" +
-                "Среднее время ожидания в очереди: " + str(self.smo.WaitTime) + "\n"+
-                "Cреднее время нахождения заявки в СМО: " + str(self.smo.SMOTime) + "\n")
-        
-        
-    def MultiChannelWithQueueWithoutLen(self, channel_count):
-        self.smo.MultiChannelWithQueueWithoutLen(channel_count)
-        if(self.smo.IsStatic):
-            return ("Вероятность образования очереди: " + str(self.smo.ProbabilityQueue) + "\n"
-                    "Среднее время ожидания в очереди: " + str(self.smo.WaitTime) + "\n"+
-                    "Среднее число заявок в СМО: " + str(self.smo.Lsmo) + "\n" +
-                    "Cреднее время нахождения заявки в СМО: " + str(self.smo.SMOTime) + "\n"+
-                    "Среднее число занятых каналов: " + str(self.smo.AverageChannel) + "\n")
-        else: 
             return "Стационарное состояние невозможно\n"
 
-    
-    
+    def MultiChannelWithFail(self, channel_count):
+        self.smo.MultiChannelWithFail(channel_count)
+        return ("Вероятность обслуживания: " + "{:2f}".format(self.smo.ProbabilityService) + "\n" +
+                "Вероятность отказа: " + "{:2f}".format(self.smo.ProbabilityFault) + "\n" +
+                "Относительнаяпропускная способность: " + "{:2f}".format(self.smo.RelativeBandwidth) + "\n" +
+                "Абсолютная пропускная способность: " + "{:2f}".format(self.smo.Bandwidth) + "\n" +
+                "Среднее число занятых каналов: " + "{:2f}".format(self.smo.AverageChannel) + "\n")
+
+    def MultiChannelWithQueue(self, channel_count, queue_length):
+
+        self.smo.MultiChannelWithQueue(channel_count, queue_length)
+        return ("Вероятность отказа: " + "{:2f}".format(self.smo.ProbabilityFault) + "\n" +
+                "Среднее число занятых каналов: " + "{:2f}".format(self.smo.AverageChannel) + "\n" +
+                "Средняя длина очереди: " + "{:2f}".format(self.smo.QueueLength) + "\n" +
+                "Среднее время ожидания в очереди: " + "{:2f}".format(self.smo.WaitTime) + "\n" +
+                "Cреднее время нахождения заявки в СМО: " + "{:2f}".format(self.smo.SMOTime) + "\n")
+
+    def MultiChannelWithQueueWithoutLen(self, channel_count):
+        self.smo.MultiChannelWithQueueWithoutLen(channel_count)
+        if (self.smo.IsStatic):
+            return ("Вероятность образования очереди: " + "{:2f}".format(self.smo.ProbabilityQueue) + "\n"
+                    "Среднее время ожидания в очереди: " + "{:2f}".format(self.smo.WaitTime) + "\n" +
+                    "Среднее число заявок в СМО: " + "{:2f}".format(self.smo.Lsmo) + "\n" +
+                    "Cреднее время нахождения заявки в СМО: " + "{:2f}".format(self.smo.SMOTime) + "\n" +
+                    "Среднее число занятых каналов: " + "{:2f}".format(self.smo.AverageChannel) + "\n")
+        else:
+            return "Стационарное состояние невозможно\n"
